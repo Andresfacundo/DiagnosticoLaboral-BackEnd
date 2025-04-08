4b8764bfd73af552d982d8dcada668c98eb44862
andresfacundo <andresfelipefacundo@gmail.com>
2025-04-08 11:17:21 -0500

    se configuran variables de entorno

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 const {Sequelize} = require('sequelize');
+require('dotenv').config();
 
-const sequelize= new Sequelize("encuesta_db", "root", "1234" , {
-    host: "localhost",
+const sequelize= new Sequelize(
+    process.env.DB_NAME || "encuenta_db",
+    process.env.DB_USER || "root",
+    process.env.DB_PASS || "1234",
+    {
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
     logging: true,
 
