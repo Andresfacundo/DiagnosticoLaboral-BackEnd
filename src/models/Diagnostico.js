87662e65a97a5256eca3ae4461f13b9bd7621dd9
andresfacundo <andresfelipefacundo@gmail.com>
2025-06-16 16:13:53 -0500

    se actualizan los modelos Diagnostico y Empleador para permitir valores nulos en campos específicos

--- src/models/Diagnostico.js
+++ src/models/Diagnostico.js
@@ -2,7 +2,7 @@ const DataTypes = require('sequelize');
 const sequelize = require('../../db');
 
 const Diagnostico = sequelize.define('Diagnostico',{
-    resultado: { type: DataTypes.JSON, allowNull: false },
+    resultado: { type: DataTypes.JSON, allowNull: true },
     creadoEn: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
 });
 
